a407c61a8d8a5ab416dc23396d71f96b0ee95f22
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-01-22 10:05:13 +0100

    switched to min.js for cryptico lib

--- static/scripts/login_page.js
+++ static/scripts/login_page.js
@@ -2,23 +2,23 @@
 /**
  * Clicking on login.
  */
-document.getElementById("button_login").addEventListener("click", function(elem, event){
-    let username = document.getElementById("input_username").value 
-    let password =  document.getElementById("input_password").value 
+document.getElementById("button_login").addEventListener("click", function (elem, event) {
+    let username = document.getElementById("input_username").value
+    let password = document.getElementById("input_password").value
     authenticate(username, password)
-    .then((res)=> { 
-        //redirect user to /user_home 
-        window.location.href = "/user_home"
-      })
+        .then((res) => {
+            //redirect user to /user_home 
+            window.location.href = "/user_home"
+        })
 })
 
 
 /**
  * Clicking on signup.
  */
-document.getElementById("button_signup").addEventListener("click", function(elem, event){
-    let username = document.getElementById("input_username").value 
-    let password =  document.getElementById("input_password").value 
+document.getElementById("button_signup").addEventListener("click", function (elem, event) {
+    let username = document.getElementById("input_username").value
+    let password = document.getElementById("input_password").value
 
     createUser(username, password)
 
